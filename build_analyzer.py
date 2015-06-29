783b18d8a77820182207a386ccf9fe2aa7f6495c
Raphaël Rigo <raphael.rigo@airbus.com>
2015-06-29 17:42:04 +0200

    sdk 1.1.32

--- build_analyzer.py
+++ build_analyzer.py
@@ -39,8 +39,8 @@ cpp_files = glob.glob( "*.cpp" );
 os.chdir( ".." )
 
 #specify the search paths/dependencies/options for gcc
-include_paths = [ "../SaleaeAnalyzerSdk-1.1.9/include" ]
-link_paths = [ "../SaleaeAnalyzerSdk-1.1.9/lib" ]
+include_paths = [ "../SaleaeAnalyzerSdk-1.1.32/include" ]
+link_paths = [ "../SaleaeAnalyzerSdk-1.1.32/lib" ]
 link_dependencies = [ "-lAnalyzer64" ] #refers to libAnalyzer.dylib or libAnalyzer.so
 
 debug_compile_flags = "-O0 -w -c -fpic -g3"
